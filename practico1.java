1a21953e07b2aadacb58cecffffb3d9c70cf6468
BewigAlejo <bewigalejo@gmail.com>
2025-09-02 21:14:30 -0300

    punto 1 y 2

--- practico1.java
+++ practico1.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
+
 public class practico1 {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-    }
+   
+   public static void main(String[] args) {
+     
+    /*
+    
+    Punto 1: 
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingresa el primer numero");
+        float numero1 = entrada.nextFloat();
+
+        System.out.println("Ingresa el segundo numero");
+        float numero2 = entrada.nextFloat();
+
+        float suma = numero1 + numero2;
+        System.out.println("La suma de los dos numeros es " + suma);
+
+        entrada.close(); 
+
+
+        
+    Punto 2:
+
+    Scanner entrada = new Scanner(System.in);
+
+    System.out.println("Ingrese el numero que quiere elevar al cuadrado");
+    float numero = entrada.nextFloat();
+
+    float numeroElevado = numero * numero;
+
+    System.out.println("El numero que ingreso era" + numero + "su cuadrado es " + numeroElevado);
+
+    */
+   }
+   
 }
